ad98d1de92f84fdbd63403e9cb02239e70be2940
Abdul Kholik <akl859349@gmail.com>
2025-03-14 21:52:00 +0700

    fixed to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,15 @@
 </div>
-<script src="../assets/vendor/jquery/jquery.min.js"></script>
+<!-- Footer -->
+<footer class="footer bg-primary py-3">
+    <div class="container text-center">
+        <span class="text-white">© <span id="currentYear"></span> Sistem Informasi Management Salassika. All rights reserved.</span>
+    </div>
+</footer>
+
+<script>
+    // Automatically update the year in the footer
+    document.getElementById('currentYear').textContent = new Date().getFullYear();
+</script><script src="../assets/vendor/jquery/jquery.min.js"></script>
 <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- Core plugin JavaScript-->
 <script src="../assets/vendor/jquery-easing/jquery.easing.min.js"></script>
